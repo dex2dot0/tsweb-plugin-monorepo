e9cb8de5c9b6a8e6ca565d1413dc81469a7543ae
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2025-03-15 00:10:40 -0500

    Add sourcemaps for prod bundle

--- esbuild.split.mjs
+++ esbuild.split.mjs
@@ -7,7 +7,7 @@ await esbuild
     outdir: './dist/prod', // Output directory instead of a single file
     platform: 'browser', // Target platform
     format: 'iife', // Generate IIFE output format
-    sourcemap: false, // Enable source maps
+    sourcemap: true, // Enable source maps
     minify: true, // Minify the output (set to true for production)
     target: ['esnext'], // JavaScript language target
     outExtension: { '.js': '.js' }, // Keep .js extension for output files
